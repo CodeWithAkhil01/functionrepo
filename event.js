f41c766325b2491b149d8b5cc25f8b6d17aee5af
CodeWithAkhil01 <akhilakmm@gmail.com>
2024-07-02 16:44:08 +0530

    add new abc

---
+++ event.js
@@ -0,0 +1,29 @@
+// let btn1 = document.querySelector(".btn");
+// btn1.addEventListener("click",()=>{
+// console.log("btnn was clicked");
+// })
+
+// same event p multiple kaam krwa skte h...
+// btn1.addEventListener("click",(e)=>{
+// console.log("btnn2 was clicked");
+// console.log(e);         // output== event object aa jayga
+// console.log(e.type);
+// }) //    output== click
+
+
+// PRACTISE Q == CHANGE BUTTON TO DARK WHEN CLICKED AND LIGHT MODE WHEN CLICKED AGAIN
+// let modebtn = document.querySelector("#mode");
+// let curMode = "light" //dark
+// modebtn.addEventListener("click",()=>{
+//     if(curMode === "light"){
+//         curMode = "dark";
+//         document.querySelector("body").style.backgroundColor = "black"
+//     }
+//     else{
+//         curMode = "light";
+//         document.querySelector("body").style.backgroundColor = "white"
+//     }
+//     console.log(curMode);
+
+    // console.log("you are trying to change mode");
+//})
